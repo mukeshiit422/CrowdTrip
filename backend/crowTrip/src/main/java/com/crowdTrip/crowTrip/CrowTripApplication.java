b8aadb8ae0f09689170ea8f6659374b29d037d39
mukesh-kumar <mukeshiitrpr@gmail.com>
2025-11-01 18:18:42 +0530

    Initialize backend set up

---
+++ backend/crowTrip/src/main/java/com/crowdTrip/crowTrip/CrowTripApplication.java
@@ -0,0 +1,13 @@
+package com.crowdTrip.crowTrip;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrowTripApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrowTripApplication.class, args);
+	}
+
+}
---
+++ backend/crowTrip/src/test/java/com/crowdTrip/crowTrip/CrowTripApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crowdTrip.crowTrip;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrowTripApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
